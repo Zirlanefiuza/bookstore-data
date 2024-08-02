9253fea05f0f35bf4e17a712f35e6c64f75cceb6
William Amos <williamamos2016@gmail.com>
2024-08-02 09:23:34 -0300

    ajeite uma linha que estava errada

--- routes/books.js
+++ routes/books.js
@@ -29,7 +29,7 @@ booksRouter.get("/books/:id", async (req, res) => {
 booksRouter.delete("/books/:id", async (req, res) => {
   const idBooks = req.params.id;
   try {
-    const books = await Pet.findOne({ where: { id: idBooks } });
+    const books = await books.findOne({ where: { id: idBooks } });
     if (books) {
       await books.destroy();
       res.json({ message: "Book removido com sucesso." });

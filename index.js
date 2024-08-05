0fcefbc76e9128b11bf1d8a02def078b7b368ef3
William Amos <williamamos2016@gmail.com>
2024-08-05 09:09:23 -0300

    correção de import

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import { connection, authenticate } from "./config/database.js"; 
 import express from "express";
 import { clientesRouter } from "./routes/clientes.js";
-// import { booksRouter } from "./routes/clientes.js";
+import { booksRouter } from "./routes/books.js";
 
 authenticate(connection).then(() => {
-    connection.sync();
+    connection.sync({});
 });
 
 const app = express();
 app.use(express.json());
 
 app.use(clientesRouter);
-// app.use(booksRouter);
+app.use(booksRouter);
 
 // Rodar a aplicação
 app.listen(3001, () => {
